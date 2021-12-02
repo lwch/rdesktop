11f0466e17826117d4e7594b346aefeb4f17ca0c
lwch <lwch748@gmail.com>
2021-12-02 17:01:31 +0800

    实现截屏

--- client_linux.go
+++ client_linux.go
@@ -55,30 +55,8 @@ func (cli *osBase) Close() {
 
 func (cli *osBase) size() (image.Point, error) {
 	return cli.cli.GetSize()
-	// var gwa C.XWindowAttributes
-	// C.XGetWindowAttributes(cli.display, cli.window, &gwa)
-	// return image.Point{X: int(gwa.width), Y: int(gwa.height)}
 }
 
 func (cli *osBase) screenshot(img *image.RGBA) error {
-	size := img.Rect.Max
-	cimage := C.XGetImage(cli.display, cli.window, 0, 0, C.uint(size.X), C.uint(size.Y), C.AllPlanes, C.ZPixmap)
-	if cimage == nil {
-		return ErrGetImage
-	}
-	defer C.XDestroyImage(cimage)
-	redMask := cimage.red_mask
-	greenMask := cimage.green_mask
-	blueMask := cimage.blue_mask
-	offset := 0
-	for y := 0; y < size.Y; y++ {
-		for x := 0; x < size.X; x++ {
-			pix := C.XGetPixel(cimage, C.int(x), C.int(y))
-			img.Pix[offset+2] = uint8(pix & blueMask)
-			img.Pix[offset+1] = uint8((pix & greenMask) >> 8)
-			img.Pix[offset+0] = uint8((pix & redMask) >> 16)
-			offset += 4
-		}
-	}
-	return nil
+	return cli.cli.GetImage(img)
 }
---
+++ x11/xproto.go
@@ -0,0 +1,48 @@
+package x11
+
+import (
+	"encoding/binary"
+	"image"
+)
+
+// https://tronche.com/gui/x/xlib/graphics/XGetImage.html
+const (
+	xyBitmap  = 0 // depth 1
+	xyPixmap  = 1 // depth==drawable depth
+	zPixmap   = 2 // depth==drawable depth
+	allPlanes = 0xffffffff
+)
+
+func (cli *Client) GetImage(img *image.RGBA) error {
+	screen := cli.info.roots[0]
+	var data [20]byte
+	data[0] = 73                                              // opcode
+	data[1] = zPixmap                                         // format
+	binary.BigEndian.PutUint16(data[2:], 5)                   // length
+	binary.BigEndian.PutUint32(data[4:], uint32(screen.root)) // drawable
+	// x: 2bytes
+	// y: 2bytes
+	binary.BigEndian.PutUint16(data[12:], screen.widthInPixels)  // width
+	binary.BigEndian.PutUint16(data[14:], screen.heightInPixels) // height
+	binary.BigEndian.PutUint32(data[16:], allPlanes)
+	ret, err := cli.call(data[:])
+	if err != nil {
+		return err
+	}
+	err = errCheck(ret)
+	if err != nil {
+		return err
+	}
+	_ = ret[1] // depth
+	// TODO: depth is not 24
+	offset := 0
+	for y := 0; y < int(screen.heightInPixels); y++ {
+		for x := 0; x < int(screen.widthInPixels); x++ {
+			img.Pix[offset+2] = ret[offset+32] // b
+			img.Pix[offset+1] = ret[offset+33] // g
+			img.Pix[offset] = ret[offset+34]   // r
+			offset += 4
+		}
+	}
+	return nil
+}
