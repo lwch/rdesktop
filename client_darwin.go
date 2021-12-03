12ef7708b0bdfa5adc515e2485cc15efee51c1a1
lwch <lwch748@gmail.com>
2021-12-03 17:10:59 +0800

    支持darwin

---
+++ client_darwin.go
@@ -0,0 +1,20 @@
+package screenshot
+
+type osBase struct {
+}
+
+func (cli *osBase) init() error {
+	return nil
+}
+
+// Close close client
+func (cli *osBase) Close() {
+}
+
+func (cli *osBase) size() (image.Point, error) {
+	return image.Point{}, nil
+}
+
+func (cli *Client) screenshot(img *image.RGBA) error {
+	return nil
+}
