76650b4ecab9a9a86683f45e52f55074f5251973
lwch <lwch748@gmail.com>
2021-12-06 18:38:48 +0800

    修正绘制鼠标时的越界问题

--- x11/xfixes.go
+++ x11/xfixes.go
@@ -29,14 +29,18 @@ func (cli *Client) GetCursorImage(img *image.RGBA) error {
 	height := binary.BigEndian.Uint16(ret[14:])
 	xHot := binary.BigEndian.Uint16(ret[16:])
 	yHot := binary.BigEndian.Uint16(ret[18:])
-	x -= xHot
-	y -= yHot
+	if x >= xHot {
+		x -= xHot
+	}
+	if y >= yHot {
+		y -= yHot
+	}
 	dOffset := 32
 	offset := int(y)*img.Rect.Max.X*4 + int(x)*4
 	for dy := 0; dy < int(height); dy++ {
 		next := offset + img.Rect.Max.X*4
 		for dx := 0; dx < int(width); dx++ {
-			if ret[dOffset+3] > 0 {
+			if ret[dOffset+3] > 0 { // a
 				img.Pix[offset+2] = ret[dOffset]   // b
 				img.Pix[offset+1] = ret[dOffset+1] // g
 				img.Pix[offset] = ret[dOffset+2]   // r
